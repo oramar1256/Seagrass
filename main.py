bb93e04f1caee627f171014c8a970c9dc5eee899
oramar1256 <or.amar@mail.huji.ac.il>
2022-05-24 16:38:12 +0300

    added plain main.py file

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+from plotnine import *
+
+
+def read_data(path):
+    pass
+
+
+def boxplot_leaves_size(df):
+    pass
+
+
+def boxplot_rhizome_size(df):
+    pass
+
+
+
+
